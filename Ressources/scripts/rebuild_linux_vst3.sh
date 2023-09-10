e8b4beeb28c123f6a0b8bf55773745b72399899d
ssabug <gerype999@gmail.com>
2023-09-10 16:30:15 +0200

    DryWet implementation & JUCE project cleaning again and again and

--- Ressources/scripts/rebuild_linux_vst3.sh
+++ Ressources/scripts/rebuild_linux_vst3.sh
@@ -13,5 +13,10 @@ echo "removing old vst ${vstpath}${pluginname}/"
 rm -rf "${vstpath}${pluginname}/"
 echo "copying new vst ${buildpath}/${pluginname} to ${vstpath}"
 cp -r "${buildpath}/${pluginname}" "$vstpath"
+
+
+echo "clean build folder for github"
+make clean
 echo "coming back to dir ${currentpath}"
 cd "${currentpath}"
+
